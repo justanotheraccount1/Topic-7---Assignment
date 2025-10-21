ddc779e005170ecffd01083a1e76da503183f50a
339729139 <339729139@gotvdsb.ca>
2025-10-21 09:37:13 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Topic_7___Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random generator = new Random();
+            List<int> nums = new List<int>();
+
+            for (int i = 0; i <= 25; i++)
+            {
+                nums.Add(generator.Next(10, 21));
+            }
+            Console.WriteLine("Here is the list of numbers:");
+            for (int i = 0; i <= 25; i++)
+            {
+                Console.Write(nums[i] + " ");
+            }
+        }
+    }
+}
