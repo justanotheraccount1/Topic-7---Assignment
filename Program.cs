a7702a5d0d2de6bf16188164d76f59e72facf02a
339729139 <339729139@gotvdsb.ca>
2025-10-23 09:36:12 -0400

    done 1-7

--- Program.cs
+++ Program.cs
@@ -39,8 +39,10 @@
 
                     while (choiceNum != "q")
                     {
+                        int removedNums = 0;
+                        int countedNums = 0;
                         Console.WriteLine("Here is the list of numbers:");
-                        for (int i = 0; i <= 25; i++)
+                        for (int i = 0; i < (nums.Count); i++)
                         {
                             Console.Write(nums[i] + " ");
                         }
@@ -96,33 +98,95 @@
                             Console.WriteLine("What number would you like to remove?");
                             while (!Int32.TryParse(Console.ReadLine(), out number)) 
                             {
-                                Console.WriteLine("That is an invalid choice. Try Again:");
+                                Console.WriteLine("What number would you like to remove?");
                             }
-                            
+                            for (int i = 0; i < nums.Count; i++)
+                            {
+                                if (nums[i] == number)
+                                {
+                                    nums.RemoveAt(i);
+                                    i -= 1;
+                                    removedNums += 1;
+                                }
+                            }
+                            Console.WriteLine($"{removedNums} {number}'s were removed");
+                            Console.WriteLine("Hit ENTER to continue.");
+                            Console.ReadLine();
+                            Console.Clear();
                         }
                         else if (choiceNum == "4")
                         {
                             Console.WriteLine("You chose Add a Number");
                             Console.WriteLine("Hit ENTER to continue.");
                             Console.ReadLine();
+                            Console.WriteLine("What number would you like to add?");
+                            while (!Int32.TryParse(Console.ReadLine(), out number))
+                            {
+                                Console.WriteLine("What number would you like to add?");
+                            }
+                            nums.Add(number);
+                            Console.WriteLine($"{number} was added to the list");
+                            Console.WriteLine("Hit ENTER to continue.");
+                            Console.ReadLine();
+                            Console.Clear();
                         }
                         else if (choiceNum == "5")
                         {
                             Console.WriteLine("You chose Count the Numbers");
                             Console.WriteLine("Hit ENTER to continue.");
                             Console.ReadLine();
+                            Console.WriteLine("What number would you like to count?");
+                            while (!Int32.TryParse(Console.ReadLine(), out number))
+                            {
+                                Console.WriteLine("What number would you like to count?");
+                            }
+                            for (int i = 0; i < nums.Count; i++)
+                            {
+                                if (nums[i] == number)
+                                {
+                                    countedNums += 1;
+                                }
+                            }
+                            Console.WriteLine($"There are {countedNums} {number}'s in the list");
+                            Console.WriteLine("Hit ENTER to continue.");
+                            Console.ReadLine();
+                            Console.Clear();
                         }
                         else if (choiceNum == "6")
                         {
                             Console.WriteLine("You chose Largest Number");
                             Console.WriteLine("Hit ENTER to continue.");
                             Console.ReadLine();
+                            int max = nums[0];
+                            for (int i = 1; i < nums.Count; i++)
+                            {
+                                if (nums[i] > max)
+                                {
+                                    max = nums[i];
+                                }
+                            }
+                            Console.WriteLine($"The largest number is {max}");
+                            Console.WriteLine("Hit ENTER to continue.");
+                            Console.ReadLine();
+                            Console.Clear();
                         }
                         else if (choiceNum == "7")
                         {
                             Console.WriteLine("You chose Smallest Number");
                             Console.WriteLine("Hit ENTER to continue.");
                             Console.ReadLine();
+                            int min = nums[0];
+                            for (int i = 1; i < nums.Count; i++)
+                            {
+                                if (nums[i] < min)
+                                {
+                                    min = nums[i];
+                                }
+                            }
+                            Console.WriteLine($"The smallest number is {min}");
+                            Console.WriteLine("Hit ENTER to continue.");
+                            Console.ReadLine();
+                            Console.Clear();
                         }
                         else if (choiceNum == "8")
                         {
@@ -133,6 +197,12 @@
                         else if (choiceNum == "9")
                         {
                             Console.WriteLine("You chose Most Frequently Occuring Value");
+                            Console.WriteLine("Hit ENTER to continue.");
+                            Console.ReadLine();
+                        }
+                        else if (choiceNum == "q")
+                        {
+                            Console.WriteLine("You chose Quit");
                             Console.WriteLine("Hit ENTER to continue.");
                             Console.ReadLine();
                         }
